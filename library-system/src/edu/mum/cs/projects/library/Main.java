d2d4de97b8789c115b29c8907db333e3d3bff080
Moustafa <moustafa.sayed66@gmail.com>
2018-06-07 15:53:12 -0500

    changes after pull

--- library-system/src/edu/mum/cs/projects/library/Main.java
+++ library-system/src/edu/mum/cs/projects/library/Main.java
@@ -14,10 +14,8 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
-=======
+
 		System.out.println("I'm in main method");
->>>>>>> 6d45de31061c9c16551ac2f3a284096d56399985
 	}
 
 }
